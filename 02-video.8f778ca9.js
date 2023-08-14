4d92e1be5dad3fa13457faedc6db5017c0dfc2bd
Carriedbydog <113688545+Carriedbydog@users.noreply.github.com>
2023-08-14 11:13:47 +0000

    Deploying to gh-pages from @ Carriedbydog/goit-js-hw-08@c9dcf92a16ea9a39b70a1f01d6a0f356c64e9c07 🚀

--- 02-video.8f778ca9.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector("iframe"),e=new Vimeo.Player(o);e.on("play",(function(){console.log("played the video!")})),e.getVideoTitle().then((function(o){console.log("title:",o)}));e.on("volumeChange",(function(o){console.log("qwewq")})),e.on("timeUpdate",(function(o){console.log(o)}));
-//# sourceMappingURL=02-video.8f778ca9.js.map
