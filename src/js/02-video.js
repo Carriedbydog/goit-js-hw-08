bfc98ed3fd1b81005a44ad39fda2dae71f23032e
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-08-15 16:05:15 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,34 +1,26 @@
 import Player from '@vimeo/player';
-import _ from 'lodash';
+import throttle from 'lodash.throttle';
+// =================================================================
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+// =================================================================
 
 player.on(
   'timeupdate',
-  _.throttle(function () {
-    console.log('time updated!');
-  }, 3000)
+  throttle(function (params) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(params));
+  }, 1000)
 );
 
-localStorage.setItem('videoplayer-current-time', Date.now());
-player
-  .setCurrentTime(30.456)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-    console.log(seconds + ' seconds');
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  const currentTime = JSON.parse(savedTime);
+  console.log(currentTime);
+  try {
+    player.setCurrentTime(currentTime.seconds);
+  } catch (error) {
+    console.error('Error setting current time:', error);
+  }
+}
